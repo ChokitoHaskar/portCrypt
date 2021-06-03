adb27e8a51c9100353252fca34b75e0ae0a74bef
BukanChoki <55580042+ChokitoHaskar@users.noreply.github.com>
2021-06-04 03:28:17 +0700

    Add Log Out button

--- Scenes/Home.js
+++ Scenes/Home.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import { LogBox, StyleSheet, Text, TouchableOpacity, View, } from 'react-native';
 import { globalStyles } from '../styles/Global'
+import * as firebase from 'firebase'
+import { loggingOut } from '../Scenes/API/FirebaseMethod'
 
 LogBox.ignoreAllLogs()
 
 const home = ({navigation}) => {
+
+  const logOut = () => {
+    loggingOut(),
+    navigation.replace('LoginPage')
+  }
+
   return(
-    <View style={[globalStyles.base, {flexDirection: 'row'}]}>
-      <TouchableOpacity onPress={ () => navigation.navigate('ShiftCipher')}>
-        <View style={[styles.button, {backgroundColor: '#CC3F0C'}]}>
-          <Text style={{color:'white'}}>Shift</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={ () => navigation.navigate('AffineCipher')}>
-        <View style={[styles.button, {backgroundColor: '#9A6D38'}]}>
-          <Text style={{color:'white'}}>Affine</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={ () => navigation.navigate('VigenereCipher') }>
-        <View style={[styles.button, {backgroundColor: '#33673B'}]}>
-          <Text style={{color:'white', fontSize: 12.5}}>Vigenere</Text>
-        </View>
-      </TouchableOpacity>
+    <View style={globalStyles.base}>
+      <View style={{flex:1, flexDirection: 'row', alignItems:'flex-end'}}>
+        <TouchableOpacity onPress={ () => navigation.navigate('ShiftCipher')}>
+          <View style={[styles.button, {backgroundColor: '#CC3F0C'}]}>
+            <Text style={{color:'white'}}>Shift</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={ () => navigation.navigate('AffineCipher')}>
+          <View style={[styles.button, {backgroundColor: '#9A6D38'}]}>
+            <Text style={{color:'white'}}>Affine</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={ () => navigation.navigate('VigenereCipher') }>
+          <View style={[styles.button, {backgroundColor: '#33673B'}]}>
+            <Text style={{color:'white', fontSize: 12.5}}>Vigenere</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={{flex: 1, marginTop:10}}>
+        <TouchableOpacity onPress={logOut}>
+          <View style={[styles.button, {backgroundColor: '#33673B'}]}>
+            <Text style={{color:'white', fontSize: 12.5}}>Log Out</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
