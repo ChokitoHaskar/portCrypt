d417c837e36ab4020f31704ee2ae9b96451b9cf1
BukanChoki <55580042+ChokitoHaskar@users.noreply.github.com>
2021-06-03 23:40:26 +0700

    Testing Firebase Method

---
+++ Scenes/API/FirebaseMethod.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase'
+import 'firebase/firestore'
+import { alert } from 'react-native'
+
+export async function registration(email, username, password) {
+    console.log('registered')
+}
+
+export async function login(username, password) {
+    console.log('logged in')
+}
