ed1b7224f66b2af1bf4a298d3e3c53f62fefeca9
BukanChoki <55580042+ChokitoHaskar@users.noreply.github.com>
2021-06-03 23:40:01 +0700

    Adding firebase to App

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import * as React from 'react';
+import * as firebase from 'firebase';
+import keys from './config/keys';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Routes from './routes/Routes';
 import globalStyles from './styles/Global';
 
-export default function App() {
+
+export default function App() {  
+  if (!firebase.apps.length) {
+      console.log('connected to firebase')
+      firebase.initializeApp(keys.firebaseConfig)
+  }
+
   return(
     <Routes />
   )
