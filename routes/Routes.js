b3057cf9486753311b66317d6937476d98f729c6
BukanChoki <55580042+ChokitoHaskar@users.noreply.github.com>
2021-06-07 06:35:00 +0700

    Add Tab navigation

--- routes/Routes.js
+++ routes/Routes.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
+import * as firebase from 'firebase';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
 import Home from '../Scenes/Home';
 import affineCipher from '../Scenes/CipherAffine'
@@ -8,10 +10,55 @@ import shiftCipher from '../Scenes/CipherShift'
 import vigenereCipher from '../Scenes/CipherVigenere';
 import loginPage from '../Scenes/Auth/Login';
 import registerPage from '../Scenes/Auth/Register';
-import loadingScreen from '../Scenes/Loading'
-
+import loadingScreen from '../Scenes/Loading';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
+
+function HomeTabs() {
+    return(
+        <Stack.Navigator>
+            <Stack.Screen
+                name='Home'
+                component={Home}
+                options={{ headerTitleAlign:'center', title: 'Port-Crypt'}}/>
+            <Stack.Screen
+                    name='ShiftCipher'
+                    component={shiftCipher}
+                    options={{ title: 'Shift Cipher' }}/>
+                <Stack.Screen
+                    name='AffineCipher'
+                    component={affineCipher}
+                    options={{ title: 'Affine Cipher' }}/>
+                <Stack.Screen
+                    name='VigenereCipher'
+                    component={vigenereCipher}
+                    options={{ title: 'Vigenere Cipher' }}/>
+        </Stack.Navigator>
+    )
+}
+
+function ProfileTabs() {
+    
+}
+
+function Root() {
+    return(
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="Home"
+                    component={HomeTabs}
+                    options={{ tabBarLabel:'Home' }}/>
+                <Tab.Screen
+                    name="Profile"
+                    component={HomeTabs}
+                    options={{ tabBarLabel:'Profile' }}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
+}
 
 function routes() {
     return(
@@ -39,22 +86,6 @@ function routes() {
                     component={registerPage}
                     options={{ headerShown:false }} />
                 <Stack.Screen
-                    name='Home'
-                    component={Home}
-                    options={{ headerTitleAlign:'center', title: 'Port-Crypt'}}/>
-                <Stack.Screen
-                    name='ShiftCipher'
-                    component={shiftCipher}
-                    options={{ title: 'Shift Cipher' }}/>
-                <Stack.Screen
-                    name='AffineCipher'
-                    component={affineCipher}
-                    options={{ title: 'Affine Cipher' }}/>
-                <Stack.Screen
-                    name='VigenereCipher'
-                    component={vigenereCipher}
-                    options={{ title: 'Vigenere Cipher' }}/>
-                <Stack.Screen
                     name='Loading'
                     component={loadingScreen}
                     options={{ headerShown: false }}/>
@@ -63,4 +94,4 @@ function routes() {
     )
 }
 
-export default routes
+export default Root
